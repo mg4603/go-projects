286ca630ad300b00b835e0938223f24da554d0d1
michael <michael.g.george@gmail.com>
2024-11-19 07:25:14 +0530

    Add helloHandler to web-server

--- web-server/main.go
+++ web-server/main.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
 func main() {
 	fileserver := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileserver)
