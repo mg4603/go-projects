aa5a04fc889c20009524085548cb0b73cdd57bd6
michael <michael.g.george@gmail.com>
2024-11-23 21:33:35 +0530

    Add fn getDummyMovies to movies-crud

--- movies-crud/main.go
+++ movies-crud/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -30,24 +29,25 @@ func SetJSONContentType(next http.Handler) http.Handler {
 	})
 }
 
-func GetMovies(w http.ResponseWriter, r *http.Request) {
-	if err := json.NewEncoder(w).Encode(movies); err != nil {
-		log.Printf("Error encoding JSON: %v", err)
-		http.Error(w, "An error occurred while processing your request", http.StatusInternalServerError)
-		return
+func getDummyMovies() map[string]Movie {
+	return map[string]Movie{
+		"1": {Id: "1", Isbn: "438227", Title: "Movie One",
+			Director: &Director{FirstName: "John", LastName: "Doe"}},
+		"2": {Id: "2", Isbn: "45455", Title: "Movie Two",
+			Director: &Director{FirstName: "Jane", LastName: "Doe"}},
 	}
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/movies", GetMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", GetMovie).Methods("GET")
-	r.HandleFunc("/movies/", CreateMovie).Methods("POST")
-	r.HandleFunc("/movies/{id}", UpdateMovie).Methods("PUT")
-	r.HandleFunc("/movies/{id}", DeleteMovie).Methods("DELETE")
-
-	r.HandleFunc("/movies", GetMovies).Methods("GET").Handler(SetJSONContentType(http.HandlerFunc(GetMovies)))
-
+	// r.HandleFunc("/movies/{id}", GetMovie).Methods("GET")
+	// r.HandleFunc("/movies/", CreateMovie).Methods("POST")
+	// r.HandleFunc("/movies/{id}", UpdateMovie).Methods("PUT")
+	// r.HandleFunc("/movies/{id}", DeleteMovie).Methods("DELETE")
+	//
+	// r.HandleFunc("/movies", GetMovies).Methods("GET").Handler(SetJSONContentType(http.HandlerFunc(GetMovies)))
+	//
 	fmt.Println("Starting server on port 8000")
 	if err := http.ListenAndServe(":8000", r); err != nil {
 		log.Fatal(err)
